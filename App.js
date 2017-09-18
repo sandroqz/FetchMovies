796221d2f3b3e9962aab0b678827136309c6f98f
Sandro Queiroz Jr <sandroqz@gmail.com>
2017-09-17 23:14:25 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import { ActivityIndicator, ListView, Text, View } from 'react-native'
+
+export default class FetchMovies extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isLoading: true
+    }
+  }
+
+  componentDidMount() {
+    return fetch('https://facebook.github.io/react-native/movies.json')
+      .then(response => response.json())
+      .then(responseJson => {
+        let ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 })
+        this.setState(
+          {
+            isLoading: false,
+            dataSource: ds.cloneWithRows(responseJson.movies)
+          },
+          function() {
+            // TODO:
+          }
+        )
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={{ flex: 1, paddingTop: 40 }}>
+          <ActivityIndicator />
+        </View>
+      )
+    }
+
+    return (
+      <View style={{ flex: 1, paddingTop: 40 }}>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={rowData => (
+            <Text>
+              {rowData.title}, {rowData.releaseYear}
+            </Text>
+          )}
+        />
+      </View>
+    )
+  }
+}
